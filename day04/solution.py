056b063c996d6f41249f100e1e30018c38af7219
William Porter <william@cap-rx.com>
2022-12-03 22:36:55 -0800

    Day 4

--- day04/solution.py
+++ day04/solution.py
@@ -1,11 +1,20 @@
 fully_contained = 0
+overlapped = 0
 
-with open('input.txt') as f:
+with open('input') as f:
     for line in f:
-        l1,r1,l2,r2 = line.strip().replace(',', ' ').replace('-', ' ').split()
-        
+        l1,r1,l2,r2 = map(int, line.strip().replace(',', ' ').replace('-', ' ').split())
+
+        if r1 < l2 or r2 < l1:
+            continue
+
         if (l1 >= l2 and r1 <= r2) or (l2 >= l1 and r2 <= r1):
-            print(l1,r1,l2,r2)
             fully_contained += 1
 
+        overlapped += 1
+
+# part 1
 print(fully_contained)
+
+# part 2
+print(overlapped)
