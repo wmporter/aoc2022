a6efd91cba56adecb75b6f8cc5203d34c2a58c79
William Porter <william@cap-rx.com>
2022-12-07 23:14:09 -0800

    Day 8 complete

--- day08/solution.py
+++ day08/solution.py
@@ -1,26 +1,56 @@
+def check(val, trees):
+    scenic = 0
+    visible = True
+    for tree in trees:
+        scenic += 1
+        if tree >= val:
+            visible = False
+            break
+    return visible, scenic
+
 def checkup(val, col, row):
-    return max([trees[r][col] for r in range(row)]) < val
-    
+    if row == 0:
+        return True, 0
+    return check(val, [trees[r][col] for r in range(row-1,-1,-1)])
+
 def checkdown(val, col, row):
-    return max([trees[r][col] for r in range(row+1,len(trees))]) < val
-    
+    if row == len(trees):
+        return True, 0
+    return check(val, [trees[r][col] for r in range(row+1,len(trees))])
+
 def checkleft(val, col, row):
-    return max(trees[row][:col]) < val
-    
+    if col == 0:
+        return True, 0
+    return check(val, trees[row][col-1::-1])
+
 def checkright(val, col, row):
-    return max(trees[row][col+1:]) < val
-    
+    if col == len(trees[0]):
+        return True, 0
+    return check(val, trees[row][col+1:])
+
+def checkall(val, col, row):
+    vup, sup = checkup(val, col, row)
+    vdn, sdn = checkdown(val, col, row)
+    vlt, slt = checkleft(val, col, row)
+    vrt, srt = checkright(val, col, row)
+    return (vup or vdn or vlt or vrt, sup * sdn * slt * srt)
 
 trees = []
-with open("input.txt") as f:
+with open("input") as f:
     for line in f:
         trees.append(list(line.strip()))
 
-visible = 2 * (len(trees) + len(trees[0])) - 4
-for r in range(1,len(trees[0])-1):
-    for c in range(1, len(trees)-1):
+visible = 0
+scenic = 0
+for r in range(len(trees[0])):
+    for c in range(len(trees)):
         val = trees[r][c]
-        if checkup(val, c, r) or checkdown(val, c, r) or checkleft(val, c, r) or checkright(val, c, r):
-            visible += 1
-            
+        vis, sce = checkall(val, c, r)
+        visible += vis
+        scenic = max(scenic, sce)
+
+# part 1
 print(visible)
+
+# part 2
+print(scenic)
