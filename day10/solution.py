1945836ec834a0e851dcb05a0f5cc1b192f1e27e
William Porter <william@cap-rx.com>
2022-12-09 22:34:35 -0800

    Day 10 complete

--- day10/solution.py
+++ day10/solution.py
@@ -1,22 +1,41 @@
-with open("input.txt") as f:
+def pixel(p, x):
+    if p % 40 in (x-1, x, x+1):
+        return "#"
+    return "."
+
+with open("input") as f:
     check = 20
     signal = 0
     cycle = 1
+    screen = ""
     x = 1
+
     for line in f:
         op = line.split()
+
+        screen += pixel(cycle-1, x)
+        if cycle % 40 == 0:
+            screen += "\n"
+
         if op[0] == "noop":
             if cycle == check:
                 signal += check * x
-                print(f"{check}", check*x)
                 check += 40
+
         if op[0] == "addx":
             if check in (cycle, cycle+1):
                 signal += check * x
-                print(f"{check}", check*x)
                 check += 40
             cycle += 1
-            print("x",x,"v",op[1])
+            screen += pixel(cycle-1, x)
+            if cycle % 40 == 0:
+                screen += "\n"
             x += int(op[1])
+
         cycle += 1
+
+# part 1
 print(signal)
+
+# part 2
+print(screen)
