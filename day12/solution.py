d26f6787df7f0c3f06af131ee7555a8ea33113e7
William Porter <wmporter@users.noreply.github.com>
2022-12-13 13:53:36 -0800

    Day 12 complete

--- day12/solution.py
+++ day12/solution.py
@@ -1,127 +1,79 @@
-import math
+from collections import deque
+
+def bfs(g, start):
+    
+    unexplored = deque()
+    unexplored.append(start)
+
+    visited_from = {}
+    visited_from[start[:-1]] = None
+
+    while len(unexplored) > 0:
+        cur = unexplored.popleft()
+        for neighbor in g.neighbors(cur):
+            if neighbor[:-1] not in visited_from:
+                unexplored.append(neighbor)
+                visited_from[neighbor[:-1]] = cur
+    
+    return visited_from
+
 
 class Node:
-    def __init__(self):
-        self.neighbors = []
-
-    def __repr__(self):
-        return f"Node: ({self.r}, {self.c}) el: {self.el}"
-    def add_neighbor(self, n):
-        self.neighbors.append(n)
-
-    # def valid_neighbors(self):
-    #     return filter(lambda n: self.el - n.el <= 1, self.neighbors)
-        # for neighbor in node.neighbors:
-        #     if node.el - neighbor.el <= -1
-
-class Tree:
-    root = None
-
-    def valid_neighbors(self, node):
-        return filter(lambda n: node.el - n.el >= -1, node.neighbors)
-        # for neighbor in node.neighbors:
-        #     if node.el - neighbor.el <= -1
+    def __init__(self, r, c, el):
+        self.r = r
+        self.c = c
+        self.el = el
     
-    def traverse_back(self, node, seen, step=0):
-        
-        if node.el == 1:
-            return 1
-        else:
-            minimum = math.inf
-            min_node = None
-            
-            print("neighbors", neighbors)
-            if not neighbors: return math.inf
-            for neighbor in neighbors:
-                if neighbor in seen:
-                    continue
-                path = self.traverse_back(neighbor, seen + [node], 1)
-                if path < minimum:
-                    minimum = path
-                    min_node = neighbor
-            return minimum + step
+    def tup(self):
+        return self.r, self.c, self.el
 
-    def traverse(self, node, seen, step=0):
-        print(node)
-        # if node in seen:
-        #     return math.inf
-        # elif node is end:
-        if node is end:
-            # print(node)
-            return 1
-        else:
-            # seen.append(node)
-            # path = math.inf
-            # for neighbor in self.valid_neighbors(node):
-            #     tail = self.traverse(neighbor, seen + [node])
-            #     path = min(
-            #         path = tail
-            #     # print(path)
-            # # return path
-            print(node)
-            minimum = math.inf
-            min_node = None
-            neighbors = list(self.valid_neighbors(node))
-            print("neighbors", neighbors)
-            if not neighbors: return math.inf
-            for neighbor in neighbors:
-                if neighbor in seen:
-                    continue
-                path = self.traverse(neighbor, seen + [node], 1)
-                if path < minimum:
-                    minimum = path
-                    min_node = neighbor
-            # if min_node:
-            #     print(f"{minimum=} {min_node=}")
-            return minimum + step
-            # return min(map(lambda n: self.traverse(n, seen + [node]), neighbors)) + step
+class Grid:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.grid = {}
 
-grid = []
+    def get(self, rowcol):
+        return self.grid[rowcol]
+    
+    def neighbors(self, loc):
+        (r, c, el) = loc
+        result = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+        result = filter(lambda n: 0 <= n[0] < self.h and 0 <= n[1] < self.w, result)
+        result = filter(lambda n: ord(self.get(n)[2]) <= ord(el)+1, result)
+        return map(self.get, result)
+
 with open("input") as f:
+    grid = {}
     for r, line in enumerate(f):
-        row = []
-        for c, el in enumerate(line.strip()):
-            n = Node()
-            n.r = r
-            n.c = c
+        for c, el in enumerate(line):
             if el == "S":
-                n.el = 1
+                n = (r, c, "a")
                 start = n
             elif el == "E":
-                n.el = 26
+                n = (r, c, "z")
                 end = n
             else:
-                # print(el)
-                n.el = ord(el) - ord("a") + 1
-                # if el == "a":
-                #     start_options.append(n)
-            # print(n)
-            row.append(n)
-        grid.append(row)
+                n = (r, c, el)
+            grid[(r, c)] = n
 
-R = len(grid)-1
-C = len(grid[0])-1
+    elf_map = Grid(len(line.strip()), r+1)
+    elf_map.grid.update(grid)
 
-for r in range(R+1):
-    for c in range(C+1):
-            node = grid[r][c]
-            if node is end:
-                continue
-            if r == 0:
-                node.add_neighbor(grid[r+1][c])
-            elif r == R:
-                node.add_neighbor(grid[r-1][c])
-            else:
-                node.add_neighbor(grid[r-1][c])
-                node.add_neighbor(grid[r+1][c])
+visited_from = bfs(elf_map, start)
 
-            if c == 0:
-                node.add_neighbor(grid[r][c+1])
-            elif c == C:
-                node.add_neighbor(grid[r][c-1])
-            else:
-                node.add_neighbor(grid[r][c-1])
-                node.add_neighbor(grid[r][c+1])
+# part 1
+cur = end
+steps = 0
+while cur != start:
+    steps += 1
+    cur = visited_from[cur[:-1]]
+print(steps)
 
-print(Tree().traverse(start, [start]))
-# print(Tree().traverse_back(end, [end]))
+# part 2
+cur = end
+steps = 0
+while cur[2] != 'a':
+    steps += 1
+    cur = visited_from[cur[:-1]]
+print(steps)
