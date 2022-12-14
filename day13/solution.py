528e5c5e32fcda0a949ff2431d5d99c8c1674fdb
William Porter <wmporter@users.noreply.github.com>
2022-12-13 19:40:43 -0800

    Day 13

--- day13/solution.py
+++ day13/solution.py
@@ -1,4 +1,6 @@
 import json
+from functools import cmp_to_key
+from pprint import pprint
 
 def compare(left, right):
     if isinstance(left, int) and isinstance(right, int):
@@ -27,14 +29,29 @@ def compare(left, right):
 with open("input") as f:
     pair = 1
     pairs = []
+    packets = [[[2]], [[6]]]
     for line in f:
         left = json.loads(line.strip())
+        packets.append(left)
         right = json.loads(next(f).strip())
+        packets.append(right)
         pairs.append(pair if compare(left, right) < 0 else 0)
         try:
             next(f)
         except:
             break
         pair += 1
+
+    packets.sort(key=cmp_to_key(compare))
     
+    for i, p in enumerate(packets):
+        if p == [[2]]:
+            dividers = i + 1
+        if p == [[6]]:
+            dividers *= i + 1
+
+# part 1
 print(sum(pairs))
+
+# part 2
+print(dividers)
